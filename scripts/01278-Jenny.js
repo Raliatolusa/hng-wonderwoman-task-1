88fa9173f8c061db489e7a466b94194dce7bfc4d
Raliat <66218336+Raliatolusa@users.noreply.github.com>
2020-06-04 13:51:43 +0100

    Update 01278-Jenny.js

--- scripts/01278-Jenny.js
+++ scripts/01278-Jenny.js
@@ -1,9 +1,9 @@
 myFunction();
 
 function myFunction()
- let myName = "Jennifer";
- let id = "HNG-01278";
- let language = "JavaScript";
- let email = "osejoseph33@gmail.com";
+ var myName = "Jennifer";
+ var id = "HNG-01278";
+ var language = "JavaScript";
+ var email = "osejoseph33@gmail.com";
  console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`)
 
